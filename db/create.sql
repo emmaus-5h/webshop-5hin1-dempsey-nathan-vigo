f7acb910c27211f7fb05052c479b7982859a43cc
Vigo van <92523574+vigovangemd@users.noreply.github.com>
2023-01-24 09:25:24 +0000

    VERASSING BOYS!

--- db/create.sql
+++ db/create.sql
@@ -26,11 +26,24 @@ CREATE TABLE reviews (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Air Jordan 4', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Air Jordan 4 Black Cats', 'Een echte klassieker uit 2010, super exclusief en nogsteeds een parel wereldwijd!', '816905633-0', 750.95);
+insert into products (name, description, code, price) values ('Air Jordan 1 X OFF WHITE', 'Deze schoen is niet meer weg te denken uit de sneaker industrie en is daarmee nogsteeds zo populair onder vele seleberties!', '077030122-3', 1300.00);
+insert into products (name, description, code, price) values ('Nike X Yeezy Red October', 'Deze samenwerking is voor de echte old schools onder ons. Ben jij nou fan van Kanye West en Nike dan is dit echt een schoen voor jou!', '445924201-X', 5000.75);
+insert into products (name, description, code, price) values ('Air jordan 1 Low X Cactus Jack ', 'Super gave schoen met een geweldige pasvorm wordt geleverd met exclusive goodie bag!', '693155505-7', 1450.00);
+insert into products (name, description, code, price) values ('Nike Air jordan 1 X OFF WHITE', 'Deze samenwerking heeft niet stilgezeten en bloeit nogsteeds in de fashionindustrie!', '686928463-6', 11000.50);
+insert into products (name, description, code, price) values ('Nike Dunk Low Pigeon', 'Dit is alweer een oudje uit 2005, maar bij ons nogsteeds verkrijgbaar in de allerbeste gloednieuwe staat!', '492662523-7', 20005.00);
 
-insert into reviews (auteur, opmerking, sneaker_id) values ('Josh', 'Harde patta', 1);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Josh', 'Coole patta', 1);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Emma', 'Fijne pasvorm en hele betrouwbare kwaliteit', 1);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Derrel', 'Lange levrtijd, maar product voldoed aan de omschrijving', 2);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Sophie', 'Zitten wat strak... Misschien moet ik ze nog wat langer dragen. Voor de rest hele toffe kleuren', 2);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Jayson', 'Favourite shoe', 3);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Merel', 'Toffe schoen voor mijn zoon', 3);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Mike', 'Supper product', 4);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Remco', 'Bedankt voor de goede service', 4);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Daan', 'Bedsnkt', 5);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Flynn', 'Eindelijk in mijn maat gevonden en ook nog eens snel geleverd', 5);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Joppe', 'Te klein en veel te laat geleverd...', 6);
+insert into reviews (auteur, opmerking, sneaker_id) values ('Bundy', 'Goede schoen dit', 6);
+
+SELECT * FROM products JOIN reviews ON product.id = reviews.products_id;
